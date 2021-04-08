1031d0d58927cec550ea7eb9699fa89f447022e3
silicon-valley <64067534+aaron-silicon-valley@users.noreply.github.com>
2021-04-08 10:13:53 -0500

    Initial Commit

---
+++ boredbutton/Color.swift
@@ -0,0 +1,27 @@
+//
+//  Color.swift
+//  boredbutton
+//
+//  Created by Aaron Ruban on 4/6/21.
+//
+
+import Foundation
+
+struct Color {
+    let colors = ["red", "orange", "blue", "white", "turquoise", "black", "lightGray", "brown", "cyan", "green", "magenta", "purple"]
+}
+
+
+
+
+//let myColors: [String: UIColor] = [
+//    "red": .red,
+//    "white": .white,
+//    //...
+//    "turquoise": UIColorFromRGB(0x40E0D0),
+//    "mediumTurquoise": UIColorFromRGB(0x48D1CC),
+//]
+
+
+
+
---
+++ boredbutton/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  boredbutton
+//
+//  Created by Aaron Ruban on 4/6/21.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var boredButton: UIButton!
+    @IBOutlet weak var backRound: UIImageView!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let theColor = colorManager.colors.randomElement()
+        print(theColor!)
+        let myColor = getColor(theColor!)
+        print(myColor ?? "white")
+        backRound.backgroundColor = myColor
+        }
+        
+    
+    var colorManager = Color()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        boredButton.layer.cornerRadius = 60
+        boredButton.backgroundColor = UIColor.blue
+        
+    }
+    
+    func getColor(_ name: String) -> UIColor? {
+        let selector = Selector("\(name)Color")
+        if UIColor.self.responds(to: selector) {
+            let color = UIColor.self.perform(selector).takeUnretainedValue()
+            return (color as! UIColor)
+        } else {
+            return nil
+        }
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
+///print("\(readycolor)")
+//let selector = Selector(readycolor!)
+//let myColor = UIColor.self.perform(selector).takeUnretainedValue()
+//print(myColor)
+//backRound.backgroundColor = myColor as? UIColor
+//        let selector: Selector = NSSelectorFromString(readycolor)
+//        backRound.backgroundColor = perform(UIColor?.selector)
+//        let selector: Selector = NSSelectorFromString(readycolor)
+//        backround.backgroundColor = perform(UIColor.selector)
+
+///
+
+
+
